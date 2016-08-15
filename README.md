ccf438228f147c6dd3c098c7ef04d1163f6870a9
Reece Bennett <reece.bennett1@gmail.com>
2016-08-15 11:24:34 +0100

    Transition to a more class based sytem

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Love2D Experimentation
 Some little experiments with the [Love2D](https://love2d.org) game framework.
 
-# To-Do / Ideas
+# Todo
+- Numbers in inventory
+- Classes for items/blocks
+- Movement/scrolling should be smooth
+
+# Ideas
 - Multiple taps to break blocks, make block shake when being broken
 - Craftable rafts (walk on/off like it was a tile?)
-- Movement/scrolling should be smooth
-- Numbers in inventory
-- Classes for items
--- main.lua
+++ main.lua
@@ -1,3 +1,39 @@
+function print_r ( t )  
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+end
+
+table.print = print_r
+
 -- KeyPressed and released detection
 love.keyboard.keysPressed = {}
 love.keyboard.keysReleased = {}
@@ -29,6 +65,43 @@ function love.keyboard.updateKeys()
   love.keyboard.keysReleased = {}
 end
 
+
+
+-- Class stuff
+Sprite = { sheet = nil, quad = nil }
+function Sprite:new(offX, offY, sheet)
+  o = { sheet = sheet, quad = getQuad(offX, offY, sheet) }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+Tile = { x = nil, y = nil, background = nil, object = nil, items = {} }
+function Tile:new(x, y, background)
+  o = { x = x, y = y, background = background, items = {} }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+Object = { sprite = nil, onBreak = nil, blocking = false }
+function Object:new(sprite, onBreak, blocking)
+  o = { sprite = sprite, onBreak = onBreak, blocking = blocking }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+Item = { sprite = nil, name = "unNamed" }
+function Item:new(sprite, name)
+  o = { sprite = sprite, name = name }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+
+
 function love.load()
   -- Keeps everything nice and pixely
   love.graphics.setDefaultFilter("nearest")
@@ -43,22 +116,22 @@ function love.load()
   
   -- Background tiles
   bgTiles = {}
-  bgTiles[1] = getQuad(0, 0, tileSheet)
-  bgTiles[2] = getQuad(1, 0, tileSheet)
-  bgTiles[3] = getQuad(2, 0, tileSheet)
-  bgTiles[4] = getQuad(3, 0, tileSheet)
-  bgTiles[5] = getQuad(4, 0, tileSheet)
-  bgTiles[6] = getQuad(5, 0, tileSheet)
-  
-  -- Objects
-  obTiles = {}
-  obTiles[1] = getQuad(0, 2, tileSheet)
-  --obTiles[2] = getQuad(1, 2, tileSheet)
+  bgTiles[1] = Sprite:new(0, 0, tileSheet)
+  bgTiles[2] = Sprite:new(1, 0, tileSheet)
+  bgTiles[3] = Sprite:new(2, 0, tileSheet)
+  bgTiles[4] = Sprite:new(3, 0, tileSheet)
+  bgTiles[5] = Sprite:new(4, 0, tileSheet)
+  bgTiles[6] = Sprite:new(5, 0, tileSheet)
   
   -- Items
   items = {}
-  items[1] = getQuad(0, 0, itemSheet)
-  items[2] = getQuad(1, 0, itemSheet)
+  items[1] = Item:new(Sprite:new(0, 0, itemSheet), "Stick")
+  items[2] = Item:new(Sprite:new(1, 0, itemSheet), "Log")
+  
+  -- Objects
+  objects = {}
+  objects[1] = Object:new(Sprite:new(0, 2, tileSheet), items[2], false)
+  --objects[2] = getQuad(1, 2, tileSheet)
   
   -- Initalise the map
   mapWidth = 60
@@ -76,26 +149,18 @@ function love.load()
   for x = 1, mapWidth do
     map[x] = {}
     for y = 1, mapHeight do
-      map[x][y] = love.math.random(1, #bgTiles)
+      -- Background tile selection
+      map[x][y] = Tile:new(x, y, bgTiles[love.math.random(1, #bgTiles)])
+      
+      -- Object selection
+      local num = love.math.random(-10, #objects)
+      if num > 0 then
+        map[x][y].object = objects[num]
+      end
     end
   end
-  
-  objects = {}
-  for x = 1, mapWidth do
-    objects[x] = {}
-    for y = 1, mapHeight do
-      objects[x][y] = love.math.random(-10, #obTiles)
-    end
-  end
-  
-  groundItems = {}
-  for x = 1, mapWidth do
-    groundItems[x] = {}
-    for y = 1, mapHeight do
-      groundItems[x][y] = 0
-    end
-  end
-  groundItems[2][3] = 1
+
+  table.insert(map[2][3].items, items[1])
   
   player = {
     x = 1,
@@ -111,7 +176,7 @@ function love.load()
   player.quad = getQuad(0, 0, player.img)
   
   for i = 1, 5 do
-    player.inventory[i] = { id = 0, count = 0 }
+    player.inventory[i] = { name = "", count = 0 }
   end
 end
 
@@ -128,16 +193,18 @@ end
 function checkCollision(xOff, yOff)
   xOff = xOff or 0
   yOff = yOff or 0
-  local o = objects[player.x+xOff]
-  if o ~= nil then
-    o = o[player.y+yOff]
-    if o ~= nil and o > 0 then
+  
+  if map[player.x+xOff] then
+    local ob = map[player.x+xOff][player.y+yOff]
+    if ob and ob.object then
       return true
-    else
-      return false
     end
+    return false
   end
+  return false
 end
+
+
 
 function love.update(dt)
   -- Exit on escape
@@ -200,26 +267,31 @@ function love.update(dt)
   end
   
   -- See if there is an item to pick up
-  local item = groundItems[player.x][player.y]
-  if item > 0 then
+  local square = map[player.x][player.y]
+  for i = #square.items, 1, -1 do
+    local item = square.items[i]
+    print(i, item)
+    
     local firstSame, firstEmpty = 0, 0
-    -- Find the next empty slot
-    for k,v in ipairs(player.inventory) do
-      if v.id == item then
-        firstSame = k
-      elseif v.id == 0 and firstEmpty == 0 then
-        firstEmpty = k
+    -- Loop through the inventory finding the first occurance of this item and first empty slot
+    for slot, v in ipairs(player.inventory) do
+      if v.name == item.name and firstSame == 0 then
+        firstSame = slot
+      elseif v.name == "" and firstEmpty == 0 then
+        firstEmpty = slot
       end
     end
       
     if firstSame > 0 then
+      -- If the item is somewhere in the inventory then put it there and remove from the floor
       player.inventory[firstSame].count = player.inventory[firstSame].count + 1
-      groundItems[player.x][player.y] = 0
+      table.remove(square.items, #square.items)
     elseif firstEmpty > 0 then
+      -- Else put it in the first empty slot and remove from the floor
       local slot = player.inventory[firstEmpty]
-      slot.id = item
+      slot.name = item.name
       slot.count = 1
-      groundItems[player.x][player.y] = 0
+      table.remove(square.items, #square.items)
     end
   end
     
@@ -241,29 +313,29 @@ function love.update(dt)
     if love.keyboard.isDown("up") and player.y > 1 then
       bx = player.x
       by = player.y-1
-      objects[bx][by] = 0
-      groundItems[bx][by] = 2
     end
     
     if love.keyboard.isDown("right") and player.x < mapWidth then
       bx = player.x+1
       by = player.y
-      objects[bx][by] = 0
-      groundItems[bx][by] = 2
     end
     
     if love.keyboard.isDown("down") and player.y < mapHeight then
       bx = player.x
       by = player.y+1
-      objects[bx][by] = 0
-      groundItems[bx][by] = 2
     end
     
     if love.keyboard.isDown("left") and player.x > 1 then
       bx = player.x-1
       by = player.y
-      objects[bx][by] = 0
-      groundItems[bx][by] = 2
+    end
+    
+    if bx > 0 and by > 0 then
+      local square = map[bx][by]
+      if square.object and square.object.onBreak then
+        table.insert(square.items, square.object.onBreak)
+        square.object = nil
+      end
     end
   end
 
@@ -274,24 +346,30 @@ function love.update(dt)
   love.keyboard.updateKeys()
 end
 
+
+
 function love.draw()
   local tileDisplaySize = tileSize * tileScale
   
   for x = 1, visibleTilesWidth do
     for y = 1, visibleTilesHeight do
+      local tile = map[x+(cameraX-1)][y+(cameraY-1)]
+      --[[
+      table.print(tile)
+      error()]]
       -- Draw background
-      love.graphics.draw(tileSheet, bgTiles[map[x+(cameraX-1)][y+(cameraY-1)]], (x-1) * tileDisplaySize, (y-1) * tileDisplaySize, 0, tileScale, tileScale)
+      local bg = tile.background
+      love.graphics.draw(bg.sheet, bg.quad, (x-1) * tileDisplaySize, (y-1) * tileDisplaySize, 0, tileScale, tileScale)
       
-      -- Draw objects
-      object = objects[x+(cameraX-1)][y+(cameraY-1)]
-      if object > 0 then
-        love.graphics.draw(tileSheet, obTiles[object], (x-1) * tileDisplaySize, (y-1) * tileDisplaySize, 0, tileScale, tileScale)
+      -- Draw object
+      local ob = tile.object
+      if ob then
+        love.graphics.draw(ob.sprite.sheet, ob.sprite.quad, (x-1) * tileDisplaySize, (y-1) * tileDisplaySize, 0, tileScale, tileScale)
       end
       
       -- Draw items
-      item = groundItems[x+(cameraX-1)][y+(cameraY-1)]
-      if item > 0 then
-        love.graphics.draw(itemSheet, items[item], (x-1) * tileDisplaySize, (y-1) * tileDisplaySize, 0, tileScale, tileScale)
+      for i, v in ipairs(tile.items) do
+        love.graphics.draw(v.sprite.sheet, v.sprite.quad, (x-1) * tileDisplaySize, (y-1) * tileDisplaySize, 0, tileScale, tileScale)
       end
     end
   end
@@ -333,7 +411,7 @@ function love.draw()
   
   local inv = "Inventory: "
   for i,v in ipairs(player.inventory) do
-    inv = inv .. i .. "[" .. v.id .. " x " .. v.count .. "] "
+    inv = inv .. i .. "[" .. v.name .. " x " .. v.count .. "] "
   end
   love.graphics.print(inv, 10, 40)
 end
